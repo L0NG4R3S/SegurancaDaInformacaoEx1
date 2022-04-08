1fb2489d4ab7cf3ad97d2fa83c93f6beb2874199
Roberta Maria Longares <roberta.l@procob.com>
2022-04-08 13:25:30 -0300

    Permitir cadastrar múltiplos usuários

--- MD5.java
+++ MD5.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,69 +17,105 @@ public class MD5 {
 		String usuarioAutenticacao = "";
 		String senhaAutenticacao = "";
 		
+		int option;
+		
 		Scanner lerUsuario = new Scanner(System.in);
-		  
+		
+		System.out.printf("\n###### MENU ######\n");
+		System.out.printf("Escolha uma opcao\n");
+		System.out.printf("1- Cadastre-se\n");
+		System.out.printf("2- Login\n");
+		option = lerUsuario.nextInt();
+		lerUsuario.nextLine();
+		
+		MessageDigest m = null;
+		try {
+			m = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 			
-			System.out.printf("\n###### CADASTRE-SE ######\n");
+		FileWriter arq = new FileWriter("/Users/procob/Downloads/Criptografia/autenticacao.txt", true);
+		PrintWriter gravarArq = new PrintWriter(arq);
+		FileReader reader = new FileReader("/Users/procob/Downloads/Criptografia/autenticacao.txt");
+		BufferedReader leitor = new BufferedReader(reader);
+	
 			
-			System.out.printf("\n\nInforme seu usuario (4 caracteres): \n");
-			usuarioDigitado = lerUsuario.nextLine();
-			
-			if(usuarioDigitado.length() != 4) {
-				do {
-					System.out.printf("\nO usuario deve conter 4 caracteres, tente novamente: \n");
+			switch (option) {
+				case 1 :
+					System.out.printf("\n###### CADASTRE-SE ######\n");
+					
+					System.out.printf("\nInforme seu usuario (4 caracteres): \n");
 					usuarioDigitado = lerUsuario.nextLine();
-				}while(usuarioDigitado.length() != 4);
-			}
-				  
-			System.out.printf("Informe sua senha (4 caracteres): \n");
-			senhaDigitada = lerUsuario.nextLine();
-			
-			if(senhaDigitada.length() != 4) {
-				do {
-					System.out.printf("\nA senha deve conter 4 caracteres, tente novamente: \n");
+					
+					if(usuarioDigitado.length() != 4) {
+						do {
+							System.out.printf("\nO usuario deve conter 4 caracteres, tente novamente: \n");
+							usuarioDigitado = lerUsuario.nextLine();
+						}while(usuarioDigitado.length() != 4);
+					}
+						  
+					System.out.printf("Informe sua senha (4 caracteres): \n");
 					senhaDigitada = lerUsuario.nextLine();
-				}while(senhaDigitada.length() != 4);
-			}
-			
-			System.out.printf("\n###### EFETUE LOGIN ######\n");
-			
-			System.out.printf("\n\nInforme seu usuario: \n");
-			usuarioAutenticacao = lerUsuario.nextLine();
-					  
-			System.out.printf("Informe sua senha: \n");
-			senhaAutenticacao = lerUsuario.nextLine();
-			
-			lerUsuario.close();
-			
-			MessageDigest m = null;
-			try {
-				m = MessageDigest.getInstance("MD5");
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			}
-			
-		    m.update(senhaDigitada.getBytes(),0,senhaDigitada.length());
-		    String hash = new BigInteger(1,m.digest()).toString(16);
-		    
-		    m.update(senhaAutenticacao.getBytes(),0,senhaAutenticacao.length());
-		    String hash2 = new BigInteger(1,m.digest()).toString(16);
-		    
-		    System.out.printf("MD5: %s\n", hash);
-		    System.out.printf("MD5 2: %s", hash2);
-			
-			FileWriter arq = new FileWriter("/Users/procob/Downloads/Criptografia/autenticacao.txt");
-			PrintWriter gravarArq = new PrintWriter(arq);
-			
-			gravarArq.printf("%s\n", usuarioDigitado);
-			gravarArq.printf("%s", hash);
-			
-			arq.close();
-			
-			if(usuarioDigitado.equals(usuarioAutenticacao) && hash.equals(hash2)) {
-				System.out.print("\n\nCREDENCIAIS CORRETAS");
-			}else {
-				System.out.print("\n\nCREDENCIAIS INCORRETAS");
+					
+					if(senhaDigitada.length() != 4) {
+						do {
+							System.out.printf("\nA senha deve conter 4 caracteres, tente novamente: \n");
+							senhaDigitada = lerUsuario.nextLine();
+						}while(senhaDigitada.length() != 4);
+					}
+					
+					lerUsuario.close();
+					
+					 m.update(senhaDigitada.getBytes(),0,senhaDigitada.length());
+					 String hash = new BigInteger(1,m.digest()).toString(16);
+					 
+					 System.out.printf("MD5: %s\n", hash);
+					 
+					 gravarArq.printf("%s, %s\n", usuarioDigitado, hash);
+					 
+					 arq.close();
+					 
+					 System.out.print("\nUsuário Registrado com sucesso!\n");
+					    
+					break;
+					
+				case 2:
+				
+					System.out.printf("\n###### EFETUE LOGIN ######\n");
+					
+					System.out.printf("\nInforme seu usuario: \n");
+					usuarioAutenticacao = lerUsuario.nextLine();
+							  
+					System.out.printf("Informe sua senha: \n");
+					senhaAutenticacao = lerUsuario.nextLine();
+					
+					lerUsuario.close();
+					
+				    m.update(senhaAutenticacao.getBytes(),0,senhaAutenticacao.length());
+				    String hash2 = new BigInteger(1,m.digest()).toString(16);
+
+					String linha = "";
+					String[] userFound = null;
+					do {
+						linha = leitor.readLine();
+						if(linha.contains(usuarioAutenticacao+",")) {
+							userFound = linha.split(", ");
+//							System.out.println(userFound[1]);
+//							System.out.println(userFound[0]);
+							
+							if(usuarioAutenticacao.equals(userFound[0]) && hash2.equals(userFound[1])) {
+								System.out.print("\n\nCREDENCIAIS CORRETAS");
+							}else {
+								System.out.print("\n\nCREDENCIAIS INCORRETAS");
+							}
+		
+							break;
+						}
+					}while((linha = leitor.readLine()) != null);
+					
+					
+					leitor.close();
 			}
 		   
 	}
